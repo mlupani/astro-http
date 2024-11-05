4e7a78ad17173c267a5eed4267a3a4198005d32a
Miguel Lupani <mlupani2@gmail.com>
2024-11-04 23:52:05 -0300

    remove db

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,12 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
-//import db from '@astrojs/db';
-
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [ mdx(), sitemap() ],
+  integrations: [ mdx(), sitemap()],
   output: 'hybrid',
 
   adapter: cloudflare(),
